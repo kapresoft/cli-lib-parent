a46c4218ce41ed0f5c04ca1d246634db873fa5af
Tony Lagnada <deloyz@gmail.com>
2024-01-15 22:02:17 -0800

    Initial commit: cli-lib

---
+++ cli-script-lib/src/test/java/com/kapresoft/cli/script/AppTest.java
@@ -0,0 +1,14 @@
+package com.kapresoft.cli.script;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class AppTest
+{
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertThat(true).isTrue();
+    }
+}
---
+++ install-poms.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env zsh
+
+
+_Main() {
+  local cmd
+  local parentPoms
+  local childPoms
+  parentPoms=("cli-dependencies.xml" "pom.xml")
+  childPoms=("cli-lib-starter.xml")
+
+  for pom in "${parentPoms[@]}" ; do
+      cmd="./mvnw -f $pom clean install -N"
+      echo "Executing: $cmd"
+      eval "${cmd}" || exit 1
+  done
+  for pom in "${childPoms[@]}" ; do
+      cmd="./mvnw -f $pom clean install -N"
+      echo "Executing: $cmd"
+      eval "${cmd}" || exit 1
+  done
+}
+
+_Main
